14307537509488d5b1789548b0fdf9f87176c827
bewakes <bewakepandey@gmail.com>
2020-06-25 12:27:16 +0545

    Update deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,4 @@
 
 secret=$WEBHOOK_SECRET
 
-echo WEBHOOK URL: $WEBHOOK_URL
-
-curl -X POST -H 'Content-Type: application/json' -d '{"token": "$secret"}' $WEBHOOK_URL/kosh
+curl -X POST -H 'Content-Type: application/json' -d "{\"token\": \"$secret\"}" $WEBHOOK_URL/kosh
