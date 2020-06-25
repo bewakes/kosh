44cedbf6cdad9419605e3943d59d6f1734d2b47b
bewakes <bewakepandey@gmail.com>
2020-06-25 11:39:36 +0545

    Remove test step

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 secret=$WEBHOOK_SECRET
-deploy_url=$WEBHOOK_URL
+
+echo WEBHOOK URL: $WEBHOOK_URL
 
 curl -X POST -H 'Content-Type: application/json' -d '{"token": "$secret"}' $WEBHOOK_URL/kosh
